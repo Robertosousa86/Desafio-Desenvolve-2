19c0e117e9b32d32b0cea7a029cf90d6c29619c3
Roberto Sousa <robertosousa186@gmail.com>
2022-03-05 22:16:54 -0300

    Creating server.js

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const setupApp = require('./app');
+require('dotenv').config();
+
+const PORT = process.env.HOST;
+// Graceful shutdown
+(async () => {
+  try {
+    const app = await setupApp();
+
+    const server = app.listen(PORT, () =>
+      console.info(`Server running at http://localhost:${PORT} 🚀`)
+    );
+
+    const exitSignals = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((sig) =>
+      process.on(sig, () =>
+        server.close((err) => {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+          app.database.connection.close(function () {
+            console.info('Database connection closed!');
+            process.exit(0);
+          });
+        })
+      )
+    );
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
