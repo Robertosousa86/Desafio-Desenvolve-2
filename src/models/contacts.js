eda20ac502ffe1be5ecaa22d6a0341fed17bc8eb
Roberto Sousa <robertosousa186@gmail.com>
2022-03-09 16:49:06 -0300

    Updating model

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -4,19 +4,24 @@ const schema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   // telephone não é obrigatório porque tel fixo caiu em desuso.
   phoneNumbers: {
     telephone: {
       type: String,
+      unique: true,
     },
     mobile: {
       type: String,
       required: true,
+      unique: true,
     },
   },
-  timestamps: true,
 });
+
+schema.set('timestamps', true);
+
 // Definindo um model no módulo global do mongoose.
 const Contact = mongoose.model('Contacts', schema);
 
