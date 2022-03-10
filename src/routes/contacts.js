cd0df9c38e05bc7b3cf5ebb0a1019dcdad324608
Roberto Sousa <robertosousa186@gmail.com>
2022-03-09 22:52:49 -0300

    Cretating http method update

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -10,5 +10,6 @@ const contactController = new ContactController(Contact);
 router.post('/', (req, res) => contactController.create(req, res));
 router.get('/', (req, res) => contactController.get(req, res));
 router.get('/:id', (req, res) => contactController.getById(req, res));
+router.put('/:id', (req, res) => contactController.update(req, res));
 
 module.exports = router;
